714e84323d86daf3bfe15aef74b83edf1a0fdbc8
Ismail Ozden <ismailozden100@gmail.com>
2021-02-05 23:25:31 +0100

    configuration properties updated BasePage.java added

---
+++ src/test/java/com/zerobank/pages/BasePage.java
@@ -0,0 +1,27 @@
+package com.zerobank.pages;
+
+import com.zerobank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    @FindBy(xpath = "(//*[@data-toggle='dropdown'])[2]")
+    public WebElement userName;
+
+    public BasePage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    public String getPageTitle(){
+        return Driver.get().getTitle();
+    }
+
+
+
+
+}
+
+
+
